29ded6865f8e8fa5243c923b01aeed7806da0ff2
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-03-11 17:39:01 -0300

    learning how to declare some variables

---
+++ declaring_variables.js
@@ -0,0 +1,19 @@
+// Documento criado para entender a declaração de variáveis em JavaScript
+
+// Declaração de variáveis em JavaScript
+let nome = "João";
+let idade = 25;
+let altura = 1.75;
+
+// declaração de constantes em JavaScript
+const PI = 3.14;
+const GRAVIDADE = 9.8;
+
+// Impriimindo as variáveis
+console.log(nome);
+console.log(idade);
+console.log(altura);
+
+// Imprimindo as constantes
+console.log(PI);
+console.log(GRAVIDADE);
