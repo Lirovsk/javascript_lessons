6b3cb69968b62ae7377f47bc17bdf69b54841bda
lirovsk <joao.lira@engenharia.ufjf.br>
2025-03-13 17:48:42 -0300

    First file about arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,10 @@
+/*
+Esse arquivo contém anotações sobre arrays em javascript.
+This file contains notes about the use of arrays in javascript
+*/
+// Declarando uma array
+let firstArray = [1, 2, 3, 4, 5];
+let arrayVazia = [];
+
+console.log(firstArray);
+console.log(arrayVazia);
