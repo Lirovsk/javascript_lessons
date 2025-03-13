8ac3ab7ab289460d8a32beeda21028eb0775c040
João Vítor Lira <134519352+Lirovsk@users.noreply.github.com>
2025-03-13 18:07:22 -0300

    Atualizar o arrays.js

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -3,11 +3,17 @@ Esse arquivo contém anotações sobre arrays em javascript.
 This file contains notes about the use of arrays in javascript
 */
 // Declarando uma array
+// Declaring an array 
 let firstArray = [1, 2, 3, 4, 5];
 let arrayVazia = [];
 
+// Imprimindo inteiramente as arrais criadas na seção anterior
+// Printing the entire arrays created on the previous section of the code
 console.log(firstArray);
 console.log(arrayVazia);
 
+// Imprimindo objetos de arrais por índice e Imprimindo o tamanho de uma array
+// Printing arrais objects using indicator and Printing the arrais length
 console.log(firstArray[1]);
 console.log(firstArray.length);
+
