5b0ecaa1a075d9f151d3d87c47ddc49c2c720254
lirovsk <joao.lira@engenharia.ufjf.br>
2025-03-16 11:10:45 -0300

    All basic manipulations methods covered in this file

--- Module 1/Arrays/adding_removing.js
+++ Module 1/Arrays/adding_removing.js
@@ -56,6 +56,22 @@ console.log(array5);
 // Como criar sublista de uma array
 // how to create subarrays from another array
 let array6 = [1, 2, 3, 4, 5];
-subArray = array6.slice(0,2);
+subArray = array6.slice(0,2); // Nesse caso a subarray vai conter os elementos de índice 0 e 1 da array original / In this case the subarray will contain the elements of index 0 and 1 of the original array
 console.log(subArray);
 console.log(array6);
+
+// Como adicionar elementos no ínicio de uma array
+// How to add items at the beginning of an array
+let array7 = [1, 2, 3, 4, 5];
+array7.unshift(0); // Adiciona o número 0 no início da array / Add the number 0 at the beginning of the array
+console.log(array7); // Imprime a array após a adição do número 0 / Print the array after the number 0 has been added
+
+// Como concatenar arrays
+// How to concatenate arrays
+let array8 = [1, 2, 3, 4, 5];
+let array9 = [6, 7, 8, 9, 10];
+let array10 = array8.concat(array9); // Concatena as duas arrays / Concatenate the two arrays
+console.log(array10); // Imprime a array concatenada / Print the concatenated array
+console.log(array10.length);
+
+// 
