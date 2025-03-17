597adf3483f281a5da78dd23c56c20dbdbbe0605
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-03-17 20:12:23 -0300

    First notes on the for_structure file

---
+++ Module 1/Repeating structures/for_structure.js
@@ -0,0 +1,9 @@
+/*
+Esse arquivo contém uma explicação sobre a sintaxe e outras infromações importantes sobre o método for
+This file contains an explanation about syntax and other important informations about for
+*/
+// Sintaxe básica
+// Basic syntax
+for (let i = 0; i<10; i++){
+    console.log((i+1));
+}
