682ea4dfc4fe042c139ee1ed31065bdba18b4fab
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-03-18 13:26:13 -0300

    Just to save theses first notes

---
+++ Module 1/Repeating structures/challeng_for.js
@@ -0,0 +1,15 @@
+/* 
+Esse arquivo conté a resolução de um desafio utilizando a estrutura 'for' - O desafio consiste em criar uma função que leia uma sequencia de carateres e consiga encontrar e contar
+quantas vezes a palavra aparece
+This file contains the resolutions of challenge using the loop 'for'- The challenge is tocreate a function that reads a caracter sequence and can foun and count how many times this
+ sequence appears on a given text
+*/
+let sequenceToCount = 'macaco';
+let text = "Sob o céu de outono, a cidade se vestia de nuances sutis e contornos surpreendentes. Cada esquina revelava vestígios de histórias esquecidas e momentos de leve encantamento. Em um beco tranquilo, um traço discreto na parede formava uma inscrição que passava despercebida pela maioria: macaco. A composição simples de letras ecoava, sem ostentação, os murmúrios de um passado anônimo. Enquanto os passos seguiam seu curso, esse detalhe singular se integrava à poesia silenciosa do lugar."
+let arrayToBeCounted = Array.from(sequenceToCount);
+let texteArray = Array.from(text);
+
+
+for (let i=(sequenceToCount.length-1); i<texteArray.length; i++){
+
+}
