fc31176f5a6cbd348ce6150659f38b9212e3f543
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-03-18 11:10:20 -0300

    for_structure.js comleted

--- Module 1/Repeating structures/for_structure.js
+++ Module 1/Repeating structures/for_structure.js
@@ -7,3 +7,26 @@ This file contains an explanation about syntax and other important informations 
 for (let i = 0; i<10; i++){
     console.log((i+1));
 }
+// Usando a estrutura for para contar quantos "zeros" um número tem
+// Using the for structure to count hao many "zeros" there are on a number
+let number = 100;
+let aux = 0;
+for (i=number; i>=10; i=i/10){
+    aux++; // This structure divide the number till its not grater than 10
+}
+console.log(`O número de zeros do número é ${aux}.`); // Here the number of digits before the most valuable one
+console.log(`O número de algarismos do número é ${aux+1}`); // Here is the number of digits of a number
+
+// Usando 'for' para contar a recorreência de um determinado dígito em um algarismo
+// Using 'for' to count the reconrrency of a digit in a number
+number = 494612387960104;
+let arrayNew =  Array.from(String(number)); // uma arrat é criada com base em número dado / An aray is created based on a given number
+console.log(arrayNew); // Impressão apenas para conferir a criação da array / Printing only to ensure thr array was sucscessfuly created
+let numberToBeCounted = 4;
+aux = 0; 
+for (let i=0; i<arrayNew.length; i++){
+    if (arrayNew[i]==numberToBeCounted){
+        aux++;
+    }
+} // Nota: o laço não estava funcionando porque não defini o limite baseado no tamanho da array (.length) / Note: this loop did not work at first beacause a didnt setup it based on the array length (.length)
+console.log(`O número de ${numberToBeCounted} em ${number} é ${aux}`);
