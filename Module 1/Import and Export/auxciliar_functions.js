d7cc5d4c5e4e5d859ed9658b6e59f56555d9e5e6
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-03-20 10:10:33 -0300

    This two files (auxciliar_functions and receaving_functions) were used as a test to learn more about inmpor and export

---
+++ Module 1/Import and Export/auxciliar_functions.js
@@ -0,0 +1,11 @@
+/*
+Nesse arquivo Existirão algumas funções que serão exportadas para outro script.
+In this file there will be some functions tha going to be exported to another file.
+*/
+export function get(){
+    return 11;
+}
+
+export function print(texto){
+    console.log(texto);
+}
---
+++ Module 1/Import and Export/receaving_functions.js
@@ -0,0 +1,9 @@
+import {print, get} from './auxciliar_functions.js';
+
+
+// Esse é só um teste e tem como objetivo consolidar o conhecimento em importção de funções.
+// This is just a test and its goal is to sink in all the knowledge about function imports.
+let numberToRepeat = get()
+for (let i=0; i<numberToRepeat; i++){
+    print(i);
+}
