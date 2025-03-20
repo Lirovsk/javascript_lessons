dbfc228ee718ae3ae816d9975ef343c8d3c1bd3e
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-03-20 10:42:03 -0300

    changing the return of values on the get functions

--- Module 1/Import and Export/auxciliar_functions.js
+++ Module 1/Import and Export/auxciliar_functions.js
@@ -2,8 +2,13 @@
 Nesse arquivo Existirão algumas funções que serão exportadas para outro script.
 In this file there will be some functions tha going to be exported to another file.
 */
+let i=0;
 export function get(){
-    return 11;
+    let array = [6, 1, 3, 4, 7, 5, 3];
+    let value = array[i];
+    i++
+    return value;
+    
 }
 
 export function print(texto){
