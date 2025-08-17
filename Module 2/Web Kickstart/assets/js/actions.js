8641b0508dc06bb2d373c92312533dae1ab6b6d6
lirovsk <joao.lira@engenharia.ufjf.br>
2025-08-16 23:04:37 -0300

    Adding motinos to the site

--- Module 2/Web Kickstart/assets/js/actions.js
+++ Module 2/Web Kickstart/assets/js/actions.js
@@ -1,22 +1,38 @@
+function jumpLogo(){
+    const logo = document.getElementById("logo");
+    if (!logo) return;
+    logo.style.transition = "transform 0.5s";
+    logo.style.transform = "translateY(-10px)";
+    logo.addEventListener("mouseover", () => {
+        logo.style.transform = "translateY(-10px)";
+    });
 
+    logo.addEventListener("mouseout", () =>{
+        logo.style.transform = "translateY(0)";
+    });
 
-function jumpText() {
-    const element = document.getElementById("jump");
-    if (!element) return;
-    const originalSize = parseInt(window.getComputedStyle(element).fontSize, 10);
-    const maxSize = originalSize + 5;
-    let currentSize = originalSize;
-    let direction = 1;
-    const interval = setInterval(() => {
-        currentSize += direction;
-        element.style.fontSize = currentSize + 'px';
-        if (currentSize >= maxSize || currentSize <= originalSize) {
-            direction *= -1;
-        }
-    }, 50); // Adjust speed as needed
+    setTimeout(() => {
+        logo.style.transform = "translateY(0)";
+    }, 500);
+}
+
+function jumpLink(){
+    const link = document.getElementsByClassName("link");
+        if (!link.length) return;
+        Array.from(link).forEach(link => {
+            const size = parseInt(window.getComputedStyle(link).fontSize, 10);
+            link.style.transition = "font-size 0.5s ease";
+            link.addEventListener("mouseover", () => {
+                link.style.fontSize = (size + 3) + "px";
+            });
+            link.addEventListener("mouseout", () => {
+                link.style.fontSize = size + "px";
+            });
+        });
 }
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    jumpText();
+    jumpLogo();
+    jumpLink();
 });
